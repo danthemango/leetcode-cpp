63d5556dcc4c0c34072d3fa5192d36531f9f182a
Daniel Guenther <danthemango@gmail.com>
2023-06-11 15:15:24 -0700

    updated clonegraph with string interpreter functions

--- problems/CloneGraph/main.cpp
+++ problems/CloneGraph/main.cpp
@@ -3,6 +3,9 @@
 #include <vector>
 #include <memory>
 #include <exception>
+#include <map>
+#include <queue>
+#include <sstream>
 using namespace std;
 
 // max number of nodes based on constraints, to be used for error checking
@@ -85,9 +88,7 @@ bool isNextChar(std::string input, int& i, char c) {
 // and returns the value in out_int
 // and increments i to the char after the pattern
 bool tryGetInt(std::string input, int& i, int& out_int) {
-    while(isSpace(input[i])) {
-        i++;
-    }
+    skipSpace(input, i);
 
     if(i >= input.size()) {
         return false;
@@ -158,30 +159,140 @@ bool tryGetEdges(std::string input, int& i, vector<vector<int>>& out_edges) {
     return true;
 }
 
-// returns node for given string
-Node* string2Graph(string input) {
-    if(input.size() < 2 || input[0] != '[' || input[input.size()-1] != ']') {
-        throw "input must be of the form: '[[(\\d+)+]*]'";
-    }
-    
+// returns true if an array of edges with format [[a,b],[c,d,...],...] are found
+// with 0 or more nodes and 0 or more edges
+bool tryGetEdges(std::string input, vector<vector<int>>& out_edges) {
     int i = 0;
-    int nesting = 0;
-    while(i < input.size()-1) {
-        int startEdge = -1;
-        int endEdge = -1;
+    return tryGetEdges(input, i, out_edges);
+}
 
-
+// returns node for given string, null if it cannot be parsed
+Node* string2Graph(string input) {
+    // get list of edges for each node
+    vector<vector<int>> nodeEdgeLists;
+    if(!tryGetEdges(input, nodeEdgeLists)) {
+        return NULL;
+    } else if(nodeEdgeLists.size() == 0) {
+        return NULL;
     }
 
-    // TODO
+    // create head node ptr
+    // create map which corresponds node val (int) to node ptr
+    // for each node of the edge-lists:
+    // - create new node with val
+    // - fill the map 
+    // - fill head if it's empty
 
-    return NULL; 
+    Node* head = NULL;
+    std::map<int, Node*> nodeMap;
+    int val = 1;
+    for(const vector<int>& nodeEdges: nodeEdgeLists) {
+        Node* node = new Node(val);
+        if(head == NULL) {
+            head = node;
+        }
+        nodeMap[val] = node;
+        val++;
+    }
+
+    // for each val in the edge list:
+    //     - look up the node ptr from the map and set it as current
+    //     - for each edge:
+    //         - look up corresponding ptr from the map
+    //         - add the ptr as a neighbor to the current node ptr
+
+    for(int i = 0; i < nodeEdgeLists.size(); i++) {
+        int curVal = i+1;
+        const vector<int>& nodeEdges = nodeEdgeLists[i];
+        for(const int& neighVal: nodeEdges) {
+            Node* curNode = nodeMap[curVal];
+            Node* neighNode = nodeMap[neighVal];
+            curNode->neighbors.push_back(neighNode);
+        }
+    }
+
+    // return head node ptr
+    return head; 
 }
 
 // returns a string for a given graph
-string graph2String(Node* input) {
-    // TODO
-    return "";
+string graph2String(Node* head) {
+    /*
+    create a map which corresponds a val to a node ptr
+    create a queue of node ptrs
+    add the head to the queue
+    */
+
+    if(head == NULL) {
+        return "[]";
+    }
+
+    std::map<int, Node*> nodeMap;
+    std::queue<Node*> nodeQueue;
+    nodeQueue.push(head);
+    nodeMap[head->val] = head;
+
+    /*
+    while the queue isn't empty:
+    - pop a node from the queue
+    - put val and ptr into the node map
+    - increment numNodes
+    - for each neighbor of node:
+        - if neighbor not in map, add it to the queue
+    */
+
+   int numNodes = 1;
+   while (!nodeQueue.empty()) {
+        Node* curNode = nodeQueue.front();
+        nodeQueue.pop();
+        for(Node* neighPtr : curNode->neighbors) {
+            if(nodeMap.find(neighPtr->val) == nodeMap.end()) {
+                nodeMap[neighPtr->val] = neighPtr;
+                nodeQueue.push(neighPtr);
+                numNodes++;
+            }
+        }
+   }
+
+   /*
+   for each node in the map:
+   - make a string list of the neighbors, in the form of [a,b,c,...]
+   */
+
+    std::stringstream ss;
+    ss << '[';
+    bool firstNode = true;
+    for (int i = 1; i <= numNodes; i++)
+    {
+        if (!firstNode) {
+            ss << ',';
+        } else {
+            firstNode = false;
+        }
+
+        Node *node = nodeMap[i];
+        ss << '[';
+        bool firstEdge = true;
+        for (Node *neighNode : node->neighbors)
+        {
+            if (!firstEdge)
+            {
+                ss << ',';
+            }
+            else
+            {
+                firstEdge = false;
+            }
+            ss << neighNode->val;
+        }
+        ss << ']';
+    }
+    ss << ']';
+
+    std::string result;
+    ss >> result;
+
+    return result;
 }
 
 // returns true if the two nodes are connected
@@ -196,22 +307,6 @@ bool areSame(Node* a, Node* b) {
     return false;
 }
 
-// returns a list of all nodes in a graph
-vector<Node*> getAllNodes(Node* graph) {
-    // TODO
-}
-
-// returns true if all nodes values in a graph are in a range (inclusive)
-bool allNodesInRange(Node* graph, int min, int max) {
-    vector<Node*> allNodes = getAllNodes(graph);
-    for(Node* node : allNodes) {
-        if(node->val < min || node->val > max) {
-            return false;
-        }
-    }
-    return true;
-}
-
 /*
 constraints:
 - The number of nodes in the graph is in the range [0, 100].
@@ -224,24 +319,9 @@ constraints:
 // returns true if the result was the same as expected
 bool runSolution(int testNbr, string inStr, string expStr) {
     std::cout << "running test number " << testNbr << std::endl;
-
-    int i = 0;
-    vector<vector<int>> edges;
-    bool foundEdges = tryGetEdges(inStr, i, edges);
-
-    if(!foundEdges) {
-        cout << "found no nodes" << endl;
-    } else {
-        cout << "found " << edges.size() << (edges.size() == 1 ? " node" : " nodes") << endl;
-        for(const vector<int>& edge : edges) {
-            cout << '[';
-            for(const int& part : edge) {
-                cout << part << " ";
-            }
-            cout << ']';
-            cout << endl;
-        }
-    }
+    Node* head = string2Graph(inStr);
+    std::string result = graph2String(head);
+    cout << "result: " << result << endl;
 
     // Solution mySolution;
     // Node* inNode = string2Graph(inStr);
@@ -253,7 +333,6 @@ int main() {
         int i = 1;
         runSolution(i++, "[[2,4],[1,3],[2,4],[1,3]]", "[[2,4],[1,3],[2,4],[1,3]]");
         runSolution(i++, "[[]]","[[]]");
-        runSolution(i++, "[[1,2,3,4],[5,6,4]]", "[[2,4],[1,3],[2,4],[1,3]]");
         runSolution(i++, "[]","[]");
         std::cout << "all done" << std::endl;
     } catch (std::exception& e) {
