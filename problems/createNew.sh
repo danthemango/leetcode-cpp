a3cc93a2abb72f53ec8c7085bddf9a1bedd011fd
Daniel Guenther <danthemango@gmail.com>
2023-07-04 19:16:35 -0700

    created helper script to create new directory

---
+++ problems/createNew.sh
@@ -0,0 +1,14 @@
+# create quick scaffolding for a new leetcode problem runner
+
+newProblem=$1
+
+if [ -z $newProblem ]; then
+    echo 'need new problem name'
+    exit
+fi
+
+mkdir $newProblem
+touch $newProblem/Solution.h
+echo '#include "../../common/common.h"' >> $newProblem/Solution.h
+touch $newProblem/readme.md
+cp sample_makefile $newProblem/Makefile
