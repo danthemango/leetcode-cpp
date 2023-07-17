324d8e1befdbd0cef3244c38cfbb235f4762ced9
Daniel Guenther <danthemango@gmail.com>
2023-07-16 18:42:18 -0700

    updated createNew to error on directory already created

--- problems/createNew.sh
+++ problems/createNew.sh
@@ -7,7 +7,12 @@ if [ -z $newProblem ]; then
     echo 'usage: bash createNew.sh <problem-name>'
     exit
 fi
+
 newProblem=$(echo $newProblem | sed 's/https:\/\/leetcode.com\/problems\///' | sed 's/\/submissions\/.*//' | sed 's/\/$//')
+if [ ! -d newProblem ]; then
+    echo Error: directory "$newProblem" already exists
+    exit 1
+fi
 
 echo "mkdir $newProblem"
 mkdir $newProblem
